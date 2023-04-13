0b3c46cc8d6b6b397bc276ae006dfa9c81ad18d4
tylercorbley <tylercorbley@gmail.com>
2023-04-13 19:12:49 -0400

    Fizz Buzz in revit challenge completed

--- FizzBuzz_Revit/Module01Challenge.cs
+++ FizzBuzz_Revit/Module01Challenge.cs
@@ -21,63 +21,83 @@ namespace FizzBuzz_Revit
             Document doc = uiapp.ActiveUIDocument.Document;
 
             // Your code goes here
-            var amount = 250;
-
-            //FizzBuzz Formula
-            for (int i = 0; i < amount; i++)
-            {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    //FizzBuzz;
-                }
-                else if (i % 3 == 0)
-                {
-                    //Fizz;
-                }
-                else if (i % 5 == 0)
-                {
-                    //Buzz;
-                }
-                else
-                {
-                    //i;
-                }
-            }
+            var number = 250;
+            var starting_elevation = 0;
+            var floor_height = 15;
 
             //create a transaction
             Transaction t = new Transaction(doc);
             t.Start("Doing something in Revit");
+            var fizzbuzz = 1;
+            var fizz = 1;
+            var buzz = 1;
 
-            //create a level
-            Level newLevel = Level.Create(doc, 10);
-            newLevel.Name = "My new level";
-
-            //create a plan
-            FilteredElementCollector collector1 = new FilteredElementCollector(doc);
-            collector1.OfClass(typeof(ViewFamilyType));
-
-            ViewFamilyType floorPlanVFT = null;
-            foreach (ViewFamilyType curVFT in collector1)
+            //FizzBuzz Formula
+            for (int i = 1; i < number; i++)
             {
-                if (curVFT.ViewFamily == ViewFamily.FloorPlan)
+                //create a level
+                Level newLevel = Level.Create(doc, i * floor_height);
+                newLevel.Name = "level_" + i.ToString();
+
+                if (i % 3 == 0 && i % 5 == 0)
                 {
-                    floorPlanVFT = curVFT;
-                    break;
+                    //FizzBuzz;
+                    //create a sheet
+                    FilteredElementCollector collector2 = new FilteredElementCollector(doc);
+                    collector2.OfCategory(BuiltInCategory.OST_TitleBlocks);
+
+                    ViewSheet newSheet = ViewSheet.Create(doc, collector2.FirstElement().Id);
+                    newSheet.Name = "FIZZBUZZ_" + fizzbuzz.ToString();
+                    newSheet.SheetNumber = i.ToString();
+                    fizzbuzz++;
+                }
+                else if (i % 3 == 0)
+                {
+                    //Fizz
+                    //create a plan
+                    FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+                    collector1.OfClass(typeof(ViewFamilyType));
+
+                    ViewFamilyType floorPlanVFT = null;
+                    foreach (ViewFamilyType curVFT in collector1)
+                    {
+                        if (curVFT.ViewFamily == ViewFamily.FloorPlan)
+                        {
+                            floorPlanVFT = curVFT;
+                            break;
+                        }
+                    }
+                    ViewPlan newPlan = ViewPlan.Create(doc, floorPlanVFT.Id, newLevel.Id);
+                    newPlan.Name = "Fizz_" + fizz.ToString();
+                    fizz++;
+                }
+                else if (i % 5 == 0)
+                {
+                    //Buzz;
+                    //create ceiling plan
+                    FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+                    collector1.OfClass(typeof(ViewFamilyType));
+
+                    ViewFamilyType ceilingPlanVFT = null;
+                    foreach (ViewFamilyType curVFT in collector1)
+                    {
+                        if (curVFT.ViewFamily == ViewFamily.CeilingPlan)
+                        {
+                            ceilingPlanVFT = curVFT;
+                            break;
+                        }
+                    }
+                    ViewPlan newCeilingPlan = ViewPlan.Create(doc, ceilingPlanVFT.Id, newLevel.Id);
+                    newCeilingPlan.Name = "Buzz_" + buzz.ToString();
+                    buzz++;
                 }
             }
-            ViewPlan newPlan = ViewPlan.Create(doc, floorPlanVFT.Id, newLevel.Id);
-            newPlan.Name = "my new floor plan";
-            //create a sheet
-            FilteredElementCollector collector2 = new FilteredElementCollector(doc);
-            collector2.OfCategory(BuiltInCategory.OST_TitleBlocks);
 
-            ViewSheet newSheet = ViewSheet.Create(doc, collector2.FirstElement().Id);
-            newSheet.Name = "My New Sheet";
-            newSheet.SheetNumber = "A103";
+
 
             //add view to sheet; create a viewport object
-            XYZ insPoint = new XYZ(1, 0.5, 0); //create a point
-            Viewport newViewport = Viewport.Create(doc, newSheet.Id, newPlan.Id, insPoint);
+            //XYZ insPoint = new XYZ(1, 0.5, 0); //create a point
+            //Viewport newViewport = Viewport.Create(doc, newSheet.Id, newPlan.Id, insPoint);
 
 
 
